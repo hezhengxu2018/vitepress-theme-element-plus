336706e875ac6a7244e34c645d2d554fb25823a9
hezhengxu2018 <hzx.tony@hotmail.com>
2025-12-28 19:03:59 +0800

    refactor: 移除未导出的组件的注释，完善gitignore文件

--- packages/theme/index.ts
+++ packages/theme/index.ts
@@ -11,36 +11,7 @@ import 'element-plus/theme-chalk/dark/css-vars.css'
 const EPTheme: Theme = {
   extends: VPTheme,
   Layout,
-  enhanceApp({ app, siteData }) {
-    const themeConfig = siteData.value.themeConfig
-    if (!import.meta.env.SSR) {
-      if (!themeConfig.scrollRestoration) {
-        if ('scrollRestoration' in history) {
-          history.scrollRestoration = 'manual'
-        }
-      }
-
-      else {
-        if ('scrollRestoration' in history) {
-          history.scrollRestoration = 'auto'
-        }
-      }
-
-      if ((import.meta as any).hot) {
-        let scrollPosition = 0;
-        // 监听热模块替换之前的事件
-        (import.meta as any).hot.on('vite:beforeUpdate', () => {
-          // 保存当前的滚动位置
-          scrollPosition = window.scrollY || document.documentElement.scrollTop
-        });
-
-        // 监听热模块替换之后的事件
-        (import.meta as any).hot.on('vite:afterUpdate', () => {
-          // 恢复保存的滚动位置
-          window.scrollTo(0, scrollPosition)
-        })
-      }
-    }
+  enhanceApp({ app }) {
     app.component('Bili', Bili)
     app.component('ElTag', ElTag)
     app.component('ApiTyping', ApiTyping)
@@ -56,13 +27,3 @@ export interface EPThemeConfig extends DefaultTheme.Config {
 
 export { Layout }
 export default EPTheme
-
-// export { default as Home } from './components/pages/Home.vue'
-// export { default as Doc } from './components/pages/Doc.vue'
-// export { default as SinglePage } from './components/pages/SinglePage.vue'
-// export { default as ChangeLog } from './components/pages/ChangeLog.vue'
-// export { default as Sponsor } from './components/pages/Sponsor.vue'
-// export { default as Jump } from './components/pages/Jump.vue'
-
-// 导出类型
-// export * from './types'
